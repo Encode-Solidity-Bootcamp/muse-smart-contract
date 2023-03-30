ab9c99bce9e87a34e86f45e386f8cd27fc7aae8c
leodarkseid <leodarkseid@gmail.com>
2023-03-30 23:33:28 +0100

    update mp, & contract

--- scripts/market.ts
+++ scripts/market.ts
@@ -11,24 +11,24 @@ async function main() {
     const contract = await contractFactory.deploy();
     console.log("contract deployed");
     const contractTx = await contract.deployTransaction.wait();
-    console.log(contractTx)
+    console.log(contractTx.contractAddress)
 
     //Deploy test token contract
     const tokenFactory = new TestSHIT__factory(deployer);
     console.log('Deploying contract')
     const tokenContract = await tokenFactory.deploy();
     const tokenContractTx = await tokenContract.deployTransaction.wait();
-    console.log(tokenContractTx);
+    console.log(tokenContractTx.contractAddress);
 
     //Mint Token 100 Tokens;
     const desAddress = deployer.address;
     const id = ethers.utils.parseEther("1");
     const mint_amount = ethers.utils.parseEther("100");
-    const data = ethers.utils.parseBytes32String("sd")
+    const data = "0x293232";
     
     const minter = await tokenContract.mint(desAddress, id, mint_amount,data)
     const mintTx = await minter.wait();
-    console.log(mintTx)
+    console.log(mintTx.contractAddress)
 
 
 }
