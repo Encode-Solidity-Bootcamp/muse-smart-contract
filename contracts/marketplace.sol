01ab9185e47f592961df1243b41007f12e0d9dcd
leodarkseid <leodarkseid@gmail.com>
2023-04-02 15:44:04 +0100

    polished markeplace smart contract

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -8,7 +8,6 @@ pragma solidity ^0.8.0;
 /// @custom:experimental This is an experimental contract.
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
