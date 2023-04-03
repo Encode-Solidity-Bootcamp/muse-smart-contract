b1e0763308a25fc7be41bfaec5da25e9d240b999
leodarkseid <leodarkseid@gmail.com>
2023-04-03 22:24:29 +0100

    added a few test

--- scripts/market.ts
+++ scripts/market.ts
@@ -75,7 +75,7 @@ async function main() {
     
     const amount = { value:ethers.BigNumber.from("1")};
     const tId = { value:ethers.BigNumber.from("1")};
-    const buyNFT = await contract.connect(account1).buyItem(1,10, { value: ethers.utils.parseEther("100")});
+    const buyNFT = await contract.connect(account1).buyItem(1,2, { value: ethers.utils.parseEther("100")});
     const buyNFTtx = await buyNFT.wait();
     const listedNFTt = await contract.items(1);
     console.log("Sold :",listedNFTt.sold);
@@ -95,6 +95,7 @@ async function main() {
         itemsArray.push(retrieveList)
         console.log("End of loop")
     }
+
     console.log(itemsArray);
     
 
