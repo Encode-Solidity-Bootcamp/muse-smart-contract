4d6e8ade48fd7c02c0636ae09ea95dd25c642e8e
leodarkseid <leodarkseid@gmail.com>
2023-04-04 14:22:48 +0100

    Added unlist functionality to the MP contract,updated security and also gas optimization

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -30,6 +30,7 @@ contract Marketplace is ERC1155Holder,Ownable{
     //@notice this maps the Item to the uint256 which serves as each item id
     //@dev this can be used to get the list of items in the marketplace
     mapping(uint256 => Item) public items;
+    mapping(uint256 => bool) public isItemUnlisted;
     uint256 public itemCount;
 
     //@notice this is used to check if contract is active or paused
@@ -46,10 +47,27 @@ contract Marketplace is ERC1155Holder,Ownable{
         _;
     }
 
-    //@dev A function to check if contract is paused
+    /// @dev A function to check if contract is paused
     function _checkPauseStatus() internal view virtual{
         require(!isPaused, "Marketplace is paused");
     }
+
+    /// @dev A function to check if the item is unlisted
+    function _isItemUnlisted(uint256 _itemId) internal view returns(bool) {
+        return isItemUnlisted[_itemId];
+    }
+
+    
+    
+    modifier _checkIfERC1155(address _nftAddress){
+        require(checkIfERC1155Status(_nftAddress), "Contract is not ERC1155 compliant");
+    _;
+        }
+
+    function checkIfERC1155Status(address _nftContract) internal view returns (bool) {
+        return IERC165(_nftContract).supportsInterface(type(IERC1155).interfaceId);
+    }
+
     
     //@dev Event for when Item is added and sold on the contract
     event ItemAdded(uint256 itemId, address nftContract, uint256 tokenId, uint256 amount, string name, uint256 price, address seller);
@@ -57,17 +75,20 @@ contract Marketplace is ERC1155Holder,Ownable{
 
     //@notice this contract requires permission to be granted to the contract 
     //@dev function to add NFTs to the marketPlace
-    function addItem(address _nftContract, uint256 _tokenId, uint256 _amount, string memory _name, uint256 _price) public _isPaused{
-        
+    function addItem(address _nftContract, uint256 _tokenId, uint256 _amount, string memory _name, uint256 _price) public _isPaused  _checkIfERC1155(_nftContract){
+    
+        require(_amount > 0, "Amount must be greater than zero");
         require(_nftContract != address(0), "Invalid  Nft contract address");
-        //@notice Check that the item being listed for sale is an ERC1155 token
-        //@dev this method is used because  "owner" is a state variable and cannot be called
+        /// @notice Check that the item being listed for sale is an ERC1155 token
+        /// @dev this method is used because  "owner" is a state variable and cannot be called
         require(IERC1155(_nftContract).balanceOf(msg.sender, _tokenId) >= _amount, "Only token owner can list for sale");
 
         itemCount++;
 
-        //@notice Add a new Item and add it to the items mapping
+        /// @notice Add a new Item and add it to the items mapping
         items[itemCount] = Item(_nftContract, _tokenId, _amount, _name, _price, payable(msg.sender), false);
+        /// @notice Ensure Item is Listed
+        isItemUnlisted[itemCount] = false;
 
         emit ItemAdded(itemCount, _nftContract, _tokenId, _amount, _name, _price, msg.sender);
     }
@@ -75,9 +96,14 @@ contract Marketplace is ERC1155Holder,Ownable{
     /// @param _itemId gets the item Id and _amount gets the amount to be bought from each user
     /// @dev the function buys the listed Items on the marketplace
     function buyItem(uint256 _itemId, uint256 _amount) public payable _isPaused {
-        require(items[_itemId].nftContract != address(0), "Item does not exist");
-        require(!items[_itemId].sold, "Item already sold");
+        /// @dev mapping imported into storage to save gas
+        Item storage item = items[_itemId];
+
         require(_amount > 0, "Amount must be greater than zero");
+        require(item.nftContract != address(0), "Item does not exist");
+        require(!_isItemUnlisted(_itemId), "Item is unlisted");
+        require(!item.sold, "Item already sold");
+        require(item.amount >= _amount, "You can't buy more than what's available");
 
         /// @dev This calculates the platform fees
         uint256 costprice = items[_itemId].price.mul(_amount);
@@ -86,29 +112,38 @@ contract Marketplace is ERC1155Holder,Ownable{
         uint256 totalCost = costprice.add(platformFees);
 
         require(msg.value >= totalCost, "Insufficient funds");
-        require(items[_itemId].amount >= _amount, "Insufficient stock");
+        require(item.amount >= _amount, "Insufficient stock");
 
         /// @notice Update the stock of the item
-        items[_itemId].amount -= _amount;
+        item.amount -= _amount;
 
         /// @dev Mark the item as sold if all items are sold out
-        if (items[_itemId].amount == 0) {
-            items[_itemId].sold = true;
+        if (item.amount == 0) {
+            item.sold = true;
         }
+        
         
         /// @notice Return any excess payment back to the buyer.
         if (msg.value > totalCost) {
             payable(msg.sender).transfer(msg.value.sub(totalCost));
             }
         /// @notice Transfer the NFT to the buyer
-        IERC1155(items[_itemId].nftContract).safeTransferFrom(items[_itemId].seller, msg.sender, items[_itemId].tokenId, _amount, "Transfer sent");
+        IERC1155(item.nftContract).safeTransferFrom(item.seller, msg.sender, item.tokenId, _amount, "Transfer sent");
 
         /// @notice Transfer the funds to the seller
-        items[_itemId].seller.transfer(costprice);
+        item.seller.transfer(costprice);
 
         /// @dev Tigger an event on completion
         emit ItemSold(_itemId, msg.sender);
         onERC1155Received(address(this), msg.sender, _itemId, _amount, "");
+    }
+
+    function unlistItem(uint256 _itemId) public {
+        require(!items[_itemId].sold, "Cannot unlist sold item");
+        require(items[_itemId].amount > 0, "Item with the given ID does not exist");
+        require(items[_itemId].seller == msg.sender, "Only the seller can unlist the item");
+        
+        isItemUnlisted[_itemId] = true;
     }
 
 
--- scripts/market.ts
+++ scripts/market.ts
@@ -9,18 +9,18 @@ async function main() {
 
     // This is the contract for deploying the Marketplace
     const contractFactory = new Marketplace__factory(deployer);
-    console.log("Deploying contract");
+    // console.log("Deploying contract");
     const contract = await contractFactory.deploy();
-    console.log("contract deployed");
+    // console.log("contract deployed");
     const contractTx = await contract.deployTransaction.wait();
-    console.log("Marketplace Contract:",contractTx.contractAddress)
+    // console.log("Marketplace Contract:",contractTx.contractAddress)
 
     //Deploy test token contract
     const tokenFactory = new TestSHIT__factory(deployer);
-    console.log('Deploying contract')
+    // console.log('Deploying contract')
     const tokenContract = await tokenFactory.deploy();
     const tokenContractTx = await tokenContract.deployTransaction.wait();
-    console.log("testToken Contract:",tokenContractTx.contractAddress);
+    // console.log("testToken Contract:",tokenContractTx.contractAddress);
 
     //Mint Token 100 Tokens;
     const desAddress = deployer.address;
@@ -30,29 +30,29 @@ async function main() {
     
     const minter = await tokenContract.mint(desAddress, id, mint_amount,data)
     const mintTx = await minter.wait();
-    console.log("ERC1155 NFT successfully minted");
-    console.log("Hash of Token Minting",mintTx.blockHash);
+    // console.log("ERC1155 NFT successfully minted");
+    // console.log("Hash of Token Minting",mintTx.blockHash);
     const owner = await tokenContract.owner();
-    console.log("This is the owner", owner)
-    console.log("This is the deployer", deployer.address)
+    // console.log("This is the owner", owner)
+    // console.log("This is the deployer", deployer.address)
 
     //Give Approval to Contract to safely transfer tokens
     const approval = await tokenContract.setApprovalForAll(contract.address, true);
     const approvalTx = await approval.wait();
-    console.log("Approval successful");
-    console.log("Token Approval Hash",approvalTx.blockHash);
+    // console.log("Approval successful");
+    // console.log("Token Approval Hash",approvalTx.blockHash);
 
     // pause marketplace.sol
 
     const pauseMP = await contract.connect(deployer).pauseMarketPlace();
     const pauseMPTx = await pauseMP.wait();
-    console.log("MarketPlace successfully Paused with hash !:", pauseMPTx.blockHash)
+    // console.log("MarketPlace successfully Paused with hash !:", pauseMPTx.blockHash)
 
     const checkMP = await contract.isPaused();
     if(checkMP){
         const resumeMP = await contract.connect(deployer).resumeMarketPlace();
         const resumeMPTx = await resumeMP.wait();
-        console.log("MarketPlace successfully Resumed with hash !:", resumeMPTx.blockHash)
+        // console.log("MarketPlace successfully Resumed with hash !:", resumeMPTx.blockHash)
     }
 
     //add Tokens to Marketplace
@@ -60,27 +60,27 @@ async function main() {
     
     const listTokens = await contract.connect(deployer).addItem(tokenContract.address, ethers.utils.parseEther(price.toFixed(18)), 10, "baddest Team 11", 1 );
     const listTokensTx = await listTokens.wait();
-    console.log("Token successfully added to marketplace")
-    console.log("Add token BlockHash",listTokensTx.blockHash);
+    // console.log("Token successfully added to marketplace")
+    // console.log("Add token BlockHash",listTokensTx.blockHash);
 
     //List Token listed in Marketplace
 
     const listedNFT = await contract.items(1);
-    console.log("Token detail:", listedNFT.name);
+    // console.log("Token detail:", listedNFT.name);
 
     //Attempt to buy 
     const checkBalance = await account1.getBalance();
-    console.log("account 1 Balance:",ethers.utils.formatEther(checkBalance));
-    console.log("price of NFT", listedNFT.price);
+    // console.log("account 1 Balance:",ethers.utils.formatEther(checkBalance));
+    // console.log("price of NFT", listedNFT.price);
     
     const amount = { value:ethers.BigNumber.from("1")};
     const tId = { value:ethers.BigNumber.from("1")};
     const buyNFT = await contract.connect(account1).buyItem(1,2, { value: ethers.utils.parseEther("100")});
     const buyNFTtx = await buyNFT.wait();
     const listedNFTt = await contract.items(1);
-    console.log("Sold :",listedNFTt.sold);
-    console.log("Buy Operation Successful")
-    console.log("This is the txHash of purchase", buyNFTtx.blockHash)
+    // console.log("Sold :",listedNFTt.sold);
+    // console.log("Buy Operation Successful")
+    // console.log("This is the txHash of purchase", buyNFTtx.blockHash)
 
     //retrieve the list of all items in the marketplace
 
@@ -88,15 +88,19 @@ async function main() {
     const countNumber = count.toNumber();
     const itemsArray = [];
     
-    console.log(countNumber)
+    console.log("Total Item in the list:",countNumber)
     for(let i = 1; i <= countNumber; i++ ){
         const retrieveList =await contract.items(i);
-        console.log("The items should follow");
+        // console.log("The items should follow");
         itemsArray.push(retrieveList)
-        console.log("End of loop")
+        // console.log("End of loop")
+
+        const Ay = await contract.isItemUnlisted(i);
+        console.log("Unlisted",Ay);
     }
 
     console.log(itemsArray);
+    
     
 
 
